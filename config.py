583a9612e444bdf3f83e38d329a4077f2d9b6be5
Telegram-Guyz <kswoll542@outlook.com>
2025-02-04 13:03:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,23 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "26728872")
-    API_HASH  = os.environ.get("API_HASH", "96985c2aaea6c75408528909b7e18879")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7299345692:AAEoPK2UkI85plbum9CPHaupNpnHcyCtjLc") 
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","ThejVwkHwjbsjsbwjs")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://Telegram_Guy:I6AfG9KKBJ5397xF@botstore.t3cuf.mongodb.net/?retryWrites=true&w=majority&appName=Botstore")
+    DB_NAME = os.environ.get("DB_NAME","Tg-Guy")     
+    DB_URL  = os.environ.get("DB_URL","")
 
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1705634892').split()]
+    START_PIC   = os.environ.get("START_PIC", "") #leave it empty, if you don't need a start pic
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
     # channels logs
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "The_TGguy") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002494020519"))
-    BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "-1002343229315"))
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") #channel user name without @ or https://t.me/
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) 
+    BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "")) 
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
